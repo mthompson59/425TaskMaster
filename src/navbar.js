4b398963a21a5134aa51ef8a3426891ec25f248c
John Ervin <jervin8@murraystate.edu>
2023-11-20 21:09:57 -0600

    added logo changed colors and borders and spacing

--- src/navbar.js
+++ src/navbar.js
@@ -1,11 +1,12 @@
-// Navbar.js
 import React from 'react';
 import './navbar.css';
+import logo from './logo.png'; // Update the path accordingly
 
 const Navbar = ({ onAddClick }) => {
   return (
     <div className="navbar">
-      <h1>Fortnite TODO List</h1>
+      <img src={logo} alt="Logo" className="navbar-logo" />
+      <h1>QuickTask</h1>
       <div className="navbar-buttons">
         <button className="add-button" onClick={onAddClick}>
           Add
